807ac518740e96d614e62b478356df567a9c9df7
Mikael <melkonyan2311@icloud.com>
2020-12-27 13:32:09 +0200

    Project created

---
+++ Introducing SwiftUI/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mikael Melkonyan on 27.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
